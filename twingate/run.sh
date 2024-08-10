f3971540e5d5ecd29cd00288a7a8bb7247685e21
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-08-09 22:26:16 -0500

    Update run.sh

--- twingate/run.sh
+++ twingate/run.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bashio
 
+NETWORK=$(bashio::config 'network')
 ACCESS_TOKEN=$(bashio::config 'access_token')
+REFRESH_TOKEN=$(bashio::config 'refresh_token')
 
 echo "info entered:"
+echo "network: $NETWORK"
 echo "access_token: $ACCESS_TOKEN"
+echo "refresh_token: $REFRESH_TOKEN"
+
+
+curl "https://binaries.twingate.com/connector/setup.sh" | sudo \
+  TWINGATE_ACCESS_TOKEN="" \ 
+  TWINGATE_REFRESH_TOKEN="" \ 
+  TWINGATE_LOG_ANALYTICS="v2" \
+  TWINGATE_NETWORK="feilhouse" \
+  TWINGATE_LABEL_DEPLOYED_BY="linux" bash
 
