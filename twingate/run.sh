252efd4df670ac43c6dd5f12c83c66188c4eb5b0
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-08-09 22:45:59 -0500

    Update run.sh

--- twingate/run.sh
+++ twingate/run.sh
@@ -3,11 +3,13 @@
 NETWORK=$(bashio::config 'network')
 ACCESS_TOKEN=$(bashio::config 'access_token')
 REFRESH_TOKEN=$(bashio::config 'refresh_token')
+LOGS=$(bashio::config 'local_connection_logs')
 
 echo "info entered:"
 echo "network: ${NETWORK}"
 echo "access_token: ${ACCESS_TOKEN}"
 echo "refresh_token: ${REFRESH_TOKEN}"
+echo "log: ${LOGS}"
 
 
 curl "https://binaries.twingate.com/connector/setup.sh" | sudo \
