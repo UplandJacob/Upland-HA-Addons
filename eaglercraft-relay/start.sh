88702ee7e61391c5bf2edf9da22b5f7d9318452c
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-10-12 08:04:13 -0500

    Update start.sh

--- eaglercraft-relay/start.sh
+++ eaglercraft-relay/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/with-contenv bashio
-
+#get config
 RELAY_CONFIG=$(bashio::config 'relayConfig')
-
+#set config file
 echo -e "[EaglerSPRelay]\n$RELAY_CONFIG" > relayConfig.ini
-
+#start turn/stun server
 turnserver --daemon --no-auth -n
-
+#start relay
 ./run.sh
