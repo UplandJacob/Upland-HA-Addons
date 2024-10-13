7d1617218786d22bd7edfc4a82106e597ddf9e2d
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-10-13 14:45:47 -0500

    Update start.sh

--- eag-relay/start.sh
+++ eag-relay/start.sh
@@ -6,7 +6,7 @@ echo:
 #get config
 RELAY_CONFIG=$(bashio::config 'relayConfig')
 #set config file
-echo -e "[EaglerSPRelay]\n$RELAY_CONFIG" > relayConfig.ini
+echo -e "[EaglerSPRelay]\nport: 6969\naddress: 0.0.0.0\n$RELAY_CONFIG" > relayConfig.ini
 
 echo "relayConfig.ini:"
 cat relayConfig.ini
