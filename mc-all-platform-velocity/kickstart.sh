71909cf4b3d611236d34ff26293a691ccd16ca95
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-10-17 15:02:52 -0500

    Update kickstart.sh

--- mc-all-platform-velocity/kickstart.sh
+++ mc-all-platform-velocity/kickstart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-java -jar velocity.jar &
+
+java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar &
 # Get the process ID (PID) of the last background command
 pid=$!
 
