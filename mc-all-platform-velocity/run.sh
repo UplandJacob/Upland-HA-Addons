9f02e94114cd45d40143e6277ea7988afd1298a8
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-11-10 16:04:09 -0600

    improve logging

--- mc-all-platform-velocity/run.sh
+++ mc-all-platform-velocity/run.sh
@@ -10,20 +10,24 @@ echo ""
 VEL_ROOT_CONFIG=$(bashio::config 'rootConfig')
 VEL_SERVERS=$(bashio::config 'servers')
 VEL_SERV_ATT_JOIN_ORD=$(jq --raw-output '.serverAttemptJoinOrder' $CONFIG_PATH)
-echo $VEL_SERV_ATT_JOIN_ORD
 # main section
 echo ""
+logGreen "velocity rootConfig JSON:"
 echo $VEL_ROOT_CONFIG
 echo ""
-# vel_root_config=$(echo "$VEL_ROOT_CONFIG" | jq -r 'to_entries | .[] | "\(.key) = \(.value)"')
 vel_root_config=$(echo "$VEL_ROOT_CONFIG" | jq -r 'to_entries | .[] | "\(.key) = \((if .value | type == "string" then "\"\(.value)\"" else .value end))"')
+logGreen "velocity rootConfig formatted:"
 echo $vel_root_config
 echo ""
 # '[servers]' section
 echo ""
+logGreen "velocity servers raw:"
 echo $VEL_SERVERS
+echo ""
 vel_servers_JSON=$(echo $VEL_SERVERS | sed 's/}/},/g' | sed '$ s/,$//' | awk '{print "[" $0 "]"}')
+logGreen "velocity servers JSON:"
 echo $vel_servers_JSON
+echo ""
 vel_servers=""
 vel_servers_length=$(echo "$vel_servers_JSON" | jq '. | length')
 for (( i=0; i<$vel_servers_length; i++ )); do
@@ -32,15 +36,22 @@ for (( i=0; i<$vel_servers_length; i++ )); do
   vel_servers+="$name = \"$addr\"\n"
   vel_servers+="\n"
 done
+logGreen "velocity servers formatted:"
 echo $vel_servers
+echo ""
 
+logGreen "velocity serverAttemptJoinOrder JSON array:"
+echo $VEL_SERV_ATT_JOIN_ORD
+echo ""
 vel_serv_ord=""
 vel_serv_ord_length=$(echo "$VEL_SERV_ATT_JOIN_ORD" | jq '. | length')
 for (( i=0; i<$vel_serv_ord_length; i++ )); do
   name=$(echo "$VEL_SERV_ATT_JOIN_ORD" | jq -r ".[$i]")
   vel_serv_ord+="    \"$name\",\n"
 done
+logGreen "velocity serverAttemptJoinOrder formatted:"
 echo $vel_serv_ord
+echo ""
 
 echo -e "config-version = \"2.7\"\nbind = \"0.0.0.0:25565\"\n$vel_root_config\n[servers]\n$vel_servers\n\ntry = [\n$vel_serv_ord\n]\n" > velocity.toml
 logGreen "velocity.toml:"
