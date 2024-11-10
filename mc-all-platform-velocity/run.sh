6d74fe0a0b0ca94c3f04d2fcb78f6101bbf1f306
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-11-10 15:08:44 -0600

    config gen fixes

--- mc-all-platform-velocity/run.sh
+++ mc-all-platform-velocity/run.sh
@@ -9,20 +9,23 @@ VEL_ROOT_CONFIG=$(bashio::config 'rootConfig')
 VEL_SERVERS=$(bashio::config 'servers')
 VEL_SERV_ATT_JOIN_ORD=$(bashio::config 'serverAttemptJoinOrder')
 # main section
-vel_root_config=$(echo "$VEL_ROOT_CONFIG" | jq -r 'to_entries | .[] | "\(.key): \(.value)"')
+vel_root_config=$(echo "$VEL_ROOT_CONFIG" | jq -r 'to_entries | .[] | "\(.key) = \(.value)"')
 # '[servers]' section
+echo $VEL_SERVERS
 vel_servers_JSON=$(echo $VEL_SERVERS | sed 's/}/},/g' | sed '$ s/,$//' | awk '{print "[" $0 "]"}')
-servers=""
-servers_length=$(echo "$vel_servers_JSON" | jq '. | length')
-for (( i=0; i<$servers_length; i++ )); do
+echo $vel_servers_JSON
+vel_servers=""
+vel_servers_length=$(echo "$vel_servers_JSON" | jq '. | length')
+for (( i=0; i<$vel_servers_length; i++ )); do
   name=$(echo "$vel_servers_JSON" | jq -r ".[$i].name")
   addr=$(echo "$vel_servers_JSON" | jq -r ".[$i].address")
-  relays+="$name = address=$addr\n"
-  relays+="\n"
+  vel_servers+="$name = address=$addr\n"
+  vel_servers+="\n"
 done
+echo $vel_servers
 
 
-echo -e "config-version = \"2.7\"\nbind = \"0.0.0.0:25565\"\n$vel_root_config\n[servers]\n$servers" > velocity.toml
+echo -e "config-version = \"2.7\"\nbind = \"0.0.0.0:25565\"\n$vel_root_config\n[servers]\n$vel_servers" > velocity.toml
 logGreen "velocity.toml:"
 cat velocity.toml
 
