6bfb89f127088bac18c6203046f475cd7e00d653
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-11-12 11:34:22 -0600

    Update run.sh

--- mc-all-platform-velocity/run.sh
+++ mc-all-platform-velocity/run.sh
@@ -150,6 +150,4 @@ logLine
 logLine
 
 
-
-
 java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -Deaglerxvelocity.stfu=true -jar velocity.jar
