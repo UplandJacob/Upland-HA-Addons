61dfe08f6f0f658c8321d699b555827e4cb6c102
UplandJacob2 <169054231+UplandJacob2@users.noreply.github.com>
2024-11-13 19:23:22 -0600

    Update run.sh

--- mc-all-platform-velocity/run.sh
+++ mc-all-platform-velocity/run.sh
@@ -148,6 +148,5 @@ cat plugins/eaglerxvelocity/authservice.yml
 
 ####### -------------------------- finalize -------------------------------------
 logLine
-logLine
 logGreen "Starting..............."
 java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -Deaglerxvelocity.stfu=true -jar velocity.jar
