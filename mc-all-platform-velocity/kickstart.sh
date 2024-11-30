772d828f6fc0a3cf62f4ae45e12d5e83b4511a29
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-11-29 18:54:39 -0600

    Update kickstart.sh

--- mc-all-platform-velocity/kickstart.sh
+++ mc-all-platform-velocity/kickstart.sh
@@ -4,8 +4,28 @@ java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalV
 # Get the process ID (PID) of the last background command
 pid=$!
 
+# Function to stop the process when the specific string is found in the log
+stop_when_string_logged() {
+  while IFS= read -r line; do
+    echo "$line"
+    if [[ "$line" == *"Run /geyser help for help!"* ]]; then
+      kill $pid
+      echo "Process stopped."
+      break
+    fi
+  done
+}
 
-duration=15
 
-sleep $duration
-kill $pid
+
+#duration=7
+
+#sleep $duration
+#kill $pid
+
+
+stop_when_string_logged < <(tail -f nohup.out)
+
+wait $pid
+
+rm /plugins/floodgate/key.pem
