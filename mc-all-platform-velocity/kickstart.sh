813ec52d37f300e79b8e9771e730b065234f327b
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-11-29 18:59:42 -0600

    Update kickstart.sh

--- mc-all-platform-velocity/kickstart.sh
+++ mc-all-platform-velocity/kickstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar &
+java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -Deaglerxvelocity.stfu=true -jar velocity.jar &
 # Get the process ID (PID) of the last background command
 pid=$!
 
@@ -8,7 +8,7 @@ pid=$!
 stop_when_string_logged() {
   while IFS= read -r line; do
     echo "$line"
-    if [[ "$line" == *"Run /geyser help for help!"* ]]; then
+    if [[ "$line" == *"has been downloaded and loaded!"* ]]; then
       kill $pid
       echo "Process stopped."
       break
