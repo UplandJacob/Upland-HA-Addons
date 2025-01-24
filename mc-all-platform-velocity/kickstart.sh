10b4aa9549088735741f2db83266bc06afff13ac
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2025-01-24 17:57:44 -0600

    better logging and fix comment

--- mc-all-platform-velocity/kickstart.sh
+++ mc-all-platform-velocity/kickstart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 tmpfile=$(mktemp)
 str=$1
-echo "kickstarting and looking for $str"
-# Create a screen session named "minecraft" and start the command
+echo "---------- kickstarting and looking for '$str' ----------"
+# Create a screen session named "velocity" and start the command
 screen -dmS velocity bash -c 'java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -Deaglerxvelocity.stfu=true -jar velocity.jar > '$tmpfile' 2>&1'
 
 # Function to stop the process when the specific string is found in the log
