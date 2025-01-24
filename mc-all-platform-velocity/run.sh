47e79e64bc2ef5d4a69e8272a720b0f85416f15a
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2025-01-24 17:55:06 -0600

    implement resable kickstart.sh

--- mc-all-platform-velocity/run.sh
+++ mc-all-platform-velocity/run.sh
@@ -29,28 +29,7 @@ fi
 # check for key.pem (for floodgate and geyser)
 if [[ ! -f "/config/geyser/key.pem" ]]; then
   logGreen "no key.pem file found. Temporarily starting proxy to generate one."
-  tmpfile=$(mktemp)
-  # Create a screen session named "minecraft" and start the command
-  screen -dmS velocity bash -c 'java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -Deaglerxvelocity.stfu=true -jar velocity.jar > '$tmpfile' 2>&1'
-  
-  # Function to stop the process when the specific string is found in the log
-  stop_when_string_logged() {
-    while IFS= read -r line; do
-      echo "- $line"
-      if [[ "$line" == *"help for help!"* ]]; then
-        sleep 1
-        # Send the "stop" command to the "velocity" screen session
-        screen -S velocity -X stuff "`echo -ne \"stop\r\"`"
-        echo "Process stopped."
-        break
-      fi
-    done
-  }
-  stop_when_string_logged < <(tail -f $tmpfile)
-  # Wait for the process to finish
-  screen -S velocity -X quit
-  rm -f $tmpfile
-  
+  ./kickstart.sh "help for help!"
   logGreen "copying key.pem to config folder..."
   cp /plugins/floodgate/key.pem /config/geyser/key.pem
 fi
