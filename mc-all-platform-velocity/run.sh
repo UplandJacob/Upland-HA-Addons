e71de46eb9fc643e824a39ce3f37a5d76eb6c3bf
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2025-02-09 20:18:11 -0600

    Fix

--- mc-all-platform-velocity/run.sh
+++ mc-all-platform-velocity/run.sh
@@ -282,7 +282,7 @@ if [[ ! -d "/config/geyser/floodgate" ]]; then
   mkdir /config/geyser/floodgate
 fi
 # local linking
-if [[ jq -r "$FLOOD_PLAYER.enable-own-linking" == true ]]; then
+if [[ jq -r "$FLOOD_PLAYER.enable-own-linking" == "true" ]]; then
   if [[ jq -r "$FLOOD_PLAYER.type" == "mysql" ]]; then
     if [[ ! -f "/config/geyser/floodgate/floodgate-mysql-database.jar" ]]; then
       logRed "missing floodgate-mysql-database.jar for Floodgate DB type 'mysql'"
