e5bb4c94a88fd19a391d018c1120da151797c040
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2025-09-24 13:47:21 -0500

    hopefully fix yq and label check

--- gitea-runner/rootfs/etc/cont-init.d/95-check-config.sh
+++ gitea-runner/rootfs/etc/cont-init.d/95-check-config.sh
@@ -3,5 +3,5 @@ bashio::log.info "Checking for config.yaml..."
 if [ ! -f /config/config.yaml ]; then
     bashio::log.info "Generating config.yaml..."
     /usr/local/bin/act_runner generate-config > /config/config.yaml
-    yq -i -y '.runner.file = "/config/.runner"' /config/config.yaml
+    yq -i '.runner.file = "/config/.runner"' /config/config.yaml
 fi
