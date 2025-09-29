1b8d7bf91e499fc3612f392abac31dac4ddc30ea
UplandJacob <jacob45feil@gmail.com>
2025-09-28 23:04:59 -0500

    squash bugs and improve logging

--- flask/rootfs/etc/s6-overlay/s6-rc.d/prepare/run.sh
+++ flask/rootfs/etc/s6-overlay/s6-rc.d/prepare/run.sh
@@ -3,34 +3,34 @@
 set -euo pipefail
 
 if [ ! -d /config ]; then
-    bashio::log.green "Creating /config folder"
+    bashio::log.green "Creating '/config' folder..."
     mkdir /config
 fi
 
 if [ ! -d /config/flask ]; then
-    bashio::log.green "Creating folder 'flask' in /config"
+    bashio::log.green "Creating folder 'flask' in '/config'..."
     mkdir -p /config/flask
 fi
 
 if [ ! -f /config/flask/requirements.txt ]; then
-    bashio::log.green "Copying default requirements.txt to /config/flask"
+    bashio::log.green "Copying default 'requirements.txt' to '/config/flask'..."
     cp /default_config/requirements.txt /config/flask/requirements.txt
 fi
 
 if [ ! -d /config/flask/.venv ]; then
-    bashio::log.green "Creating virtual environment in /config/flask"
+    bashio::log.green "Creating virtual environment in '/config/flask'..."
     python3 -m venv /config/flask/.venv
 fi
 # Activate the virtual environment
-bashio::log.green "Activating virtual environment in /config/flask"
+bashio::log.green "Activating virtual environment in '/config/flask' (.venv)..."
 source /config/flask/.venv/bin/activate
 
 # Install the required packages from the user's requirements.txt
-bashio::log.green "Installing required packages from /config/flask/requirements.txt"
+bashio::log.green "Installing required packages from '/config/flask/requirements.txt'"
 python3 -m pip install --no-cache-dir --break-system-packages -r /config/flask/requirements.txt
 
 if [ ! -f /config/flask/main.py ]; then
-    bashio::log.green "Copying default main.py to /config/flask"
+    bashio::log.green "Copying default 'main.py' to '/config/flask'"
     cp /default_config/main.py /config/flask/main.py
 fi
 
