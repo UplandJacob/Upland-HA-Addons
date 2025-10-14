45ef0482a75e5fbe96d6c655e3ee5d5d43d00996
UplandJacob <jacob45feil@gmail.com>
2025-10-13 23:27:14 -0500

    fix comment

--- flask-dev/default_config/main.py
+++ flask-dev/default_config/main.py
@@ -10,4 +10,4 @@ def index():
 
 
 if __name__ == '__main__': #   \/ required to be accessible from outside the container - can also be the hostname \/ if you only want it accessible by the HA internal network
-    app.run(debug=True, host="0.0.0.0", port=5000) # changing the port will require connecting with the hostname ('d78ad65c-flask')
+    app.run(debug=True, host="0.0.0.0", port=5000) # changing the port will require connecting with the hostname ('d78ad65c-flask-dev')
